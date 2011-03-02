9a75666844e13a89c3504764a362013731745174
clhodapp <clhodapp1@gmail.com>
2011-03-01 18:00:25 -0600

    initial commit

---
+++ src/edu/ua/cs/campustour/CampusTour.java
@@ -0,0 +1,13 @@
+package edu.ua.cs.campustour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CampusTour extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
