e02ec99ac4f6d313b042bd00855038410d56aff9
clhodapp <clhodapp1@gmail.com>
2011-03-05 18:16:02 -0600

    show on-screen zoom controls on map

--- src/edu/ua/cs/campustour/CampusTour.java
+++ src/edu/ua/cs/campustour/CampusTour.java
@@ -3,17 +3,25 @@ package edu.ua.cs.campustour;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class CampusTour extends MapActivity {
+	private MapView map;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+        initMapView();
     }
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+	
+	public void initMapView() {
+		map = (MapView) findViewById(R.id.map);
+		map.setBuiltInZoomControls(true);
 	}
 }
